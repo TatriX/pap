669c6263cf635c280c54ee025d92203ebd5aa841
TatriX <tatrics@gmail.com>
2023-08-05 11:25:10 +0200

    [wepskam] Add crazy example

--- wepskam/wepskam.c
+++ wepskam/wepskam.c
@@ -7,7 +7,7 @@
 typedef void (*proc)(void);
 
 static void
-mat_set(const int N, float mat[][N], float value) {
+mat_set(const int N, double mat[][N], double value) {
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
             mat[i][j] = value;
@@ -15,14 +15,11 @@ mat_set(const int N, float mat[][N], float value) {
     }
 }
 
-
-
 #define N 1000
-float mul1[N][N];
-float mul2[N][N];
-float res[N][N];
-
-
+double mul1[N][N];
+double mul2[N][N];
+double res[N][N];
+double tmp[N][N];
 
 static void
 benchmark(proc body) {
@@ -54,7 +51,6 @@ naive(void) {
 
 static void
 transposed(void) {
-    float tmp[N][N];
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
             tmp[i][j] = mul2[j][i];
@@ -67,6 +63,24 @@ transposed(void) {
             }
         }
     }
+}
+
+static void
+crazy(void) {
+#define CLS 64 // getconf LEVEL1_DCACHE_LINESIZE
+#define SM (CLS / sizeof (double))
+    int i, i2, j, j2, k, k2;
+    double *rmul1, *rmul2, *rres;
+    for (i = 0; i < N; i += SM)
+        for (j = 0; j < N; j += SM)
+            for (k = 0; k < N; k += SM)
+                for (i2 = 0, rres = &res[i][j],
+                         rmul1 = &mul1[i][k]; i2 < SM;
+                     ++i2, rres += N, rmul1 += N)
+                    for (k2 = 0, rmul2 = &mul2[k][j];
+                         k2 < SM; ++k2, rmul2 += N)
+                        for (j2 = 0; j2 < SM; ++j2)
+                            rres[j2] += rmul1[k2] * rmul2[j2];
 }
 
 int main(int argc, char *argv[]) {
@@ -88,4 +102,7 @@ int main(int argc, char *argv[]) {
 
     printf("# Transposed\n");
     benchmark(transposed);
+
+    printf("# Crazy\n");
+    benchmark(crazy);
 }
